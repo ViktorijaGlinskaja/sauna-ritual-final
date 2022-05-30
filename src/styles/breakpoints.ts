4aff7d0566540bf432573c8b048158930ceb8d41
VIKTORIJA GLINSKAJA <viktorijaglinskaja@gmail.com>
2022-05-30 16:07:49 +0300

    fix: fixed breakpoints according theme breakpoints

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,10 +1,17 @@
 import { useMediaQuery } from 'react-responsive';
 
-export const smMobile = '(max-width: 24rem)'; //384px
-export const mobile = '(max-width: 30rem)'; //480px
-export const tablet = '(max-width: 48rem)'; //768px
+export const sMobile = '(max-width: 16rem)'; 
+export const mobile = '(max-width: 24rem)'; 
+export const lMobile = '(max-width: 50rem)'; 
+export const sTablet = '(max-width: 59.375rem)'; 
+export const lTablet = '(max-width: 65rem)'; 
+export const sDesktop = '(max-width: 90rem)'; 
 
 export const useQuery = () => ({
+	isSmMobile: useMediaQuery({ query: mobile }),
 	isMobile: useMediaQuery({ query: mobile }),
-	isTablet: useMediaQuery({ query: tablet }),
+	isLMobile: useMediaQuery({ query: lMobile }),
+	isSmTablet: useMediaQuery({ query: sTablet }),
+	isTablet: useMediaQuery({ query: lTablet }),
+	isSmDesktop: useMediaQuery({ query: sDesktop }),
 });
